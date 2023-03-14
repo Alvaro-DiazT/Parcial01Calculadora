bea03d7c67cad89c879a6d4a928d8e42fc6d7140
Alvaro-DiazT <adiazt99@gmail.com>
2023-03-14 16:34:49 -0500

    Se agrega las funciones de cada boton de la interfaz grafica, adicional  se le modifciaciones de estilos y personalización a la aplicación

--- calculadora.py
+++ calculadora.py
@@ -2,7 +2,37 @@ from tkinter import *
 from tkinter import ttk
 import math
 
+"""--------------------------Cambiar tema de la calculadora-----------------------------------"""
+def temaOscuro(*args):
+    estilos.configure("mainframe.TFrame", background = "#010924")
 
+    estilos_label1.configure("Label1.TLabel",  background = "#010924", foreground="white")
+    estilos_label2.configure("Label2.TLabel", background="#010924", foreground="white")
+
+    estilos_botones_numeros.configure("Botones_numeros.TButton", background="#00044A", foreground="white")
+    estilos_botones_numeros.map("Botones_numeros.TButton", background=[("active", "#020A90")])
+
+    estilos_botones_borrar.configure("Botones_borrar.TButton", background="#010924", foreground="white")
+    estilos_botones_borrar.map("Botones_borrar.TButton", background=[("active", "#000AB1")])
+
+    estilos_botones_operacion.configure("Botones_operacion.TButton", background="#010924", foreground="white")
+    estilos_botones_operacion.map("Botones_operacion.TButton", background=[("active", "#000AB1")])
+def temaClaro(*args):
+    estilos.configure("mainframe.TFrame", background="#DBDBDB", foreground="black")
+
+    estilos_label1.configure("Label1.TLabel", background="#DBDBDB", foreground="black")
+    estilos_label2.configure("Label2.TLabel", background="#DBDBDB", foreground="black")
+
+    estilos_botones_numeros.configure("Botones_numeros.TButton", background="#FFFFFF", foreground="black")
+    estilos_botones_numeros.map("Botones_numeros.TButton", background=[("active", "#B9B9B9")])
+
+    estilos_botones_borrar.configure("Botones_borrar.TButton", background="#CECECE", foreground="black")
+    estilos_botones_borrar.map("Botones_borrar.TButton", background=[("active", "#858585")])
+
+    estilos_botones_operacion.configure("Botones_operacion.TButton", background="#CECECE", foreground="black")
+    estilos_botones_operacion.map("Botones_operacion.TButton", background=[("active", "#858585")])
+
+"""--------------------------Funciones para el uso de cada boton ----------------------------"""
 def ingresarValores(tecla):
     if tecla >= '0' and tecla <= '9' or tecla == '(' or tecla == ')' or tecla == '.':
         entrada2.set(entrada2.get() + tecla)
@@ -99,6 +129,22 @@ mainFrame = ttk.Frame(windowRoot, style="mainframe.TFrame")
 
 #Adaptar los widgets a la ventana principal
 mainFrame.grid(column=0, row=0, sticky= ( W, N, E, S) )
+
+#Adaptar las columnas
+mainFrame.columnconfigure(0,weight=1)
+mainFrame.columnconfigure(1,weight=1)
+mainFrame.columnconfigure(2,weight=1)
+mainFrame.columnconfigure(3,weight=1)
+
+#Adaptar las filas
+mainFrame.rowconfigure(0,weight=1)
+mainFrame.rowconfigure(1,weight=1)
+mainFrame.rowconfigure(2,weight=1)
+mainFrame.rowconfigure(3,weight=1)
+mainFrame.rowconfigure(4,weight=1)
+mainFrame.rowconfigure(5,weight=1)
+mainFrame.rowconfigure(6,weight=1)
+mainFrame.rowconfigure(7,weight=1)
 
 """----------------------------------------LABEL------------------------------------------"""
 #Estilo de los labels
@@ -203,8 +249,16 @@ button0.grid(column=1, row=7, sticky= ( W, N, E, S))
 button_igual.grid(column=2, row=7, sticky= ( W, N, E, S))
 button_logaritmo.grid(column=3, row=7, sticky= ( W, N, E, S))
 
+#Para realizar separaciones entre cada widget se utilizara un for
+for child in mainFrame.winfo_children():
+    child.grid_configure(ipady= 10, padx= 1, pady= 1)
 
-
+"""-----------------------Comandos para la utilización del teclado---------------------------"""
+windowRoot.bind("<KeyPress-o>",temaOscuro)
+windowRoot.bind("<KeyPress-c>",temaClaro)
+windowRoot.bind("<Key>", ingresarValoresTeclado)
+windowRoot.bind("<KeyPress-d>", borrar)
+windowRoot.bind("<KeyPress-q>", borrarTodo)
 
 #Mostrar la ventana principal hasta que se cierre
 windowRoot.mainloop()
