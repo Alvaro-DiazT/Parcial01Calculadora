4b8f10982075e170086ecb042f0278b8a2e2c4d9
Alvaro-DiazT <adiazt99@gmail.com>
2023-03-14 00:33:57 -0500

    Se crea la ventana principal, con sus respectivas dimensiones.

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk
+import math
+
+#Dimensionar la ventana principal
+windowRoot = Tk()
+windowRoot.title("Calculadora")
+windowRoot.geometry("+500+80")
+
+
+windowRoot.mainloop() #Mostrar la ventana principal
