81280d74f0077a7a7eba28ece14ead4e24cc1361
Alvaro-DiazT <adiazt99@gmail.com>
2023-03-14 00:50:56 -0500

    Se Agrega adaptación de la ventana principal con los widgets

--- calculadora.py
+++ calculadora.py
@@ -7,5 +7,10 @@ windowRoot = Tk()
 windowRoot.title("Calculadora")
 windowRoot.geometry("+500+80")
 
+#Adapta el tamaño de la columna y fila de la ventana principal junto con los widgets
+windowRoot.columnconfigure(0, weight= 1)
+windowRoot.rowconfigure(0, weight= 1)
+
+
 
 windowRoot.mainloop() #Mostrar la ventana principal
